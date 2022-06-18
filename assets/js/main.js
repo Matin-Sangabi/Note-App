2691bd31aa55cb2745ea46323f75522eba2de056
Matin-Sangabi <89559512+Matin-Sangabi@users.noreply.github.com>
2022-06-18 21:50:49 +0430

    note Api class added

---
+++ assets/js/main.js
@@ -0,0 +1,40 @@
+// smple notes
+
+const notes = [
+  {
+    id: 1,
+    title: "first notes",
+    body: "this is first notes",
+    updated: "2022-06-18T17:15:29.310Z",
+  },
+  {
+    id: 2,
+    title: "second notes",
+    body: "this is second notes",
+    updated: "2021-06-18T17:15:29.310Z",
+  },
+  {
+    id: 3,
+    title: "Third notes",
+    body: "this is Third notes",
+    updated: "2021-12-18T17:15:29.310Z",
+  },
+  {
+    id: 4,
+    title: "fourth notes",
+    body: "this is fourth notes",
+    updated: "2021-01-18T17:15:29.310Z",
+  },
+];
+
+class NoteApi {
+    static getAllNotes(){
+        
+    }
+    static saveNote(){
+
+    }
+    static deleteNotes(){
+
+    }
+}
